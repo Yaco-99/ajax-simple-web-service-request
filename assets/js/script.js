1f4763c2e47b0ce6d6a4feb2089d1eb637c088a9
Yaco-99 <coopman.yannick@gmail.com>
2020-10-14 10:35:01 +0200

    new button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,37 @@
-let xhr = new XMLHttpRequest();
+generate();
+function generate() {
+  let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://thatsthespir.it/api", true);
+  xhr.open("GET", "https://thatsthespir.it/api", true);
 
-xhr.onload = function () {
-  switch (this.status) {
-    case 404:
-      alert("403 FORBIDDEN - something went wrong");
-      break;
+  xhr.onload = function () {
+    switch (this.status) {
+      case 404:
+        alert("403 FORBIDDEN - something went wrong");
+        break;
 
-    case 403:
-      alert("403 FORBIDDEN - something went wrong");
-      break;
+      case 403:
+        alert("403 FORBIDDEN - something went wrong");
+        break;
 
-    case 200:
-      console.log("Succes");
-      console.log(JSON.parse(this.responseText));
-      break;
+      case 200:
+        console.log("Succes");
+        console.log(JSON.parse(this.responseText));
+        break;
+    }
+  };
+
+  xhr.send();
+
+  async function getData() {
+    let response = await fetch("https://thatsthespir.it/api");
+    let data = await response.json();
+    return data;
   }
-};
-
-xhr.send();
-
-async function getData() {
-  let response = await fetch("https://thatsthespir.it/api");
-  let data = await response.json();
-  return data;
+  getData().then((data) => {
+    document.getElementById(
+      "quote"
+    ).innerHTML = `${data.quote} <br/>-${data.author}`;
+    document.getElementById("quoteImg").src = data.photo;
+  });
 }
-getData().then((data) => {
-  document.getElementById(
-    "quote"
-  ).innerHTML = `${data.quote} <br/>-${data.author}`;
-  document.getElementById("quoteImg").src = data.photo;
-});
